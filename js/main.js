f9d892aa4c226740cad80844658749f731ab1302
Gilberto Conde <gilberto.conde@lightshift.xyz>
2023-09-20 17:02:45 +0100

    add defer to js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import Game from "./models.js";
-import Display from "./display.js";
+import Game from "/js/models.js";
+import Display from "/js/display.js";
 
 Croquet.Session.join({
   apiKey: "1r1C9u9FM5rvx2xp4daCNnHOu0sqDMFOzc09HF47y", // get your own from croquet.io/keys
