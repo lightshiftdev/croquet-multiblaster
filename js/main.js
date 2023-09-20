4799492e303457272952d0af7143df25116881c6
jmoreira-ls <joao.moreira@lightshift.xyz>
2023-09-20 16:48:51 +0100

    Update API key

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import Game from "./models.js";
 import Display from "./display.js";
 
 Croquet.Session.join({
-  apiKey: "1AC1uci95ILt22fKnuuFuBtlObEA0rne9OrHsicqe", // get your own from croquet.io/keys
+  apiKey: "1r1C9u9FM5rvx2xp4daCNnHOu0sqDMFOzc09HF47y", // get your own from croquet.io/keys
   appId: "xyz.lightshift.gilberto.conde.asteroids",
   name: Croquet.App.autoSession(),
   password: Croquet.App.autoPassword(),
